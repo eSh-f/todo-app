ef951d937354949d0fc5653ef5921862281122ac
eSh-f <gaiver97@mail.ru>
2024-11-21 12:55:09 +0500

    Заменил BrowserRouter на HashRouter для GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import ProjectBorder from './pages/ProjectBorder';
 import TaskList from './pages/TaskList';
 import Home from './pages/Home';
@@ -9,11 +9,13 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/projects' element={<ProjectBorder />} />
-        <Route path='/projects/:title' element={<TaskList />} />
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<ProjectBorder />} />
+          <Route path='/projects/:title' element={<TaskList />} />
+        </Routes>
+      </HashRouter>
     </DndProvider>
   );
 }
